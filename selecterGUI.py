1fbcad6d545d9cbc4a3bb06ee641f5ef0953d5c0
evaota <evangelionryo@gmail.com>
2018-08-15 23:21:29 +0900

    Added index.html

--- selecterGUI.py
+++ selecterGUI.py
@@ -1,18 +1,20 @@
 import RPi.GPIO as GPIO
 import time
-#from flask import Flask, render_teomplate
+from flask import Flask, render_teomplate
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
-#@app.routej('/')
-#def index():
-#	return 'Hello'
+@app.routej('/',methods=['GET','POST'])
+def index():
+	return render_template('index.html')
+	
 if __name__ == '__name__':
-#	app.debug = True
-#	app.run = (host='0.0.0.0',port=80)
-    Channel1 = 15
-    Channel2 = 14
-    Channel3 = 13
+	app.debug = True
+	app.run = (host='0.0.0.0',port=80)
+
+	Channel1 = 17
+    Channel2 = 27
+    Channel3 = 22
 
     GPIO.setmode(GPIO.BCM)
 
