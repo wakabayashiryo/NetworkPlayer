bd8dbb82e44cc7b315071c54f485a9ac6598a518
wakabayashiryo <evangelionryo@gmail.com>
2018-08-16 12:45:13 +0000

    fixed invalid indents and sytax

--- selecterGUI.py
+++ selecterGUI.py
@@ -1,32 +1,30 @@
 import RPi.GPIO as GPIO
 import time
-from flask import Flask, render_teomplate
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.routej('/',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def index():
 	return render_template('index.html')
 	
-if __name__ == '__name__':
+if __name__ == '__main__':
 	app.debug = True
-	app.run = (host='0.0.0.0',port=80)
-
+	app.run(host='0.0.0.0',port=80)
 	Channel1 = 17
-    Channel2 = 27
-    Channel3 = 22
+	Channel2 = 27
+	Channel3 = 22
 
-    GPIO.setmode(GPIO.BCM)
+	GPIO.setmode(GPIO.BCM)
 
-    GPIO.setup(Channel1,GPIO.OUT)
-    GPIO.setup(Channel2,GPIO.OUT)
-    GPIO.setup(Channel3,GPIO.OUT)
+	GPIO.setup(Channel1,GPIO.OUT)
+	GPIO.setup(Channel2,GPIO.OUT)
+	GPIO.setup(Channel3,GPIO.OUT)
+	try:
+		time.sleep(3000.0)
+		GPIO.output(Channel1,GPIO.HIGH)
+		while true:
+			pass
 
-    try:
-        time.sleep(3000.0)
-        GPIO.output(Channel1,GPIO.HIGH)
-        while true:
-            pass
-        
-    except KeyboardInterrupt:
-        GPIO.cleanuup()
+	except KeyboardInterrupt:
+		GPIO.cleanuup()
