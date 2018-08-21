4bec6a2ce1eb446fb4cae917600b10badd535e7b
wakabayashiryo <evangelionryo@gmail.com>
2018-08-21 09:45:01 +0000

    confirmed all GPIO behavier

--- selecterGUI.py
+++ selecterGUI.py
@@ -1,30 +1,18 @@
 import RPi.GPIO as GPIO
 import time
-#from flask import Flask, render_teomplate
 
-#app = Flask(__name__)
+if __name__ == '__main__':
+	Channel1 = 17
+	Channel2 = 22
+	Channel3 = 27
 
-#@app.routej('/')
-#def index():
-#	return 'Hello'
-if __name__ == '__name__':
-#	app.debug = True
-#	app.run = (host='0.0.0.0',port=80)
-    Channel1 = 15
-    Channel2 = 14
-    Channel3 = 13
+	GPIO.setmode(GPIO.BCM)
 
-    GPIO.setmode(GPIO.BCM)
+	GPIO.setup(Channel1,GPIO.OUT)
+	GPIO.setup(Channel2,GPIO.OUT)
+	GPIO.setup(Channel3,GPIO.OUT)
 
-    GPIO.setup(Channel1,GPIO.OUT)
-    GPIO.setup(Channel2,GPIO.OUT)
-    GPIO.setup(Channel3,GPIO.OUT)
+	GPIO.output(Channel2,GPIO.HIGH)
 
-    try:
-        time.sleep(3000.0)
-        GPIO.output(Channel1,GPIO.HIGH)
-        while true:
-            pass
-        
-    except KeyboardInterrupt:
-        GPIO.cleanuup()
+	time.sleep(5)     
+	GPIO.cleanup()
